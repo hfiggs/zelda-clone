0afc238c342d7d2341623d6031ecec094119a08a
Sergei Fedulov <fedulov.1@osu.edu>
2020-09-18 18:38:54 -0400

    added heart container class

---
+++ Game1/Item/HeartContainer.cs
@@ -0,0 +1,29 @@
+﻿using Game1.Sprite;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Item
+{
+    class HeartContainer : IItem
+    {
+        ISprite mySprite;
+
+        public HeartContainer()
+        {
+            mySprite = ItemSpriteFactory.Instance.CreateHeartContainerSprite();
+        }
+        public void Update()
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            mySprite.Draw(spriteBatch, position);
+        }
+    }
+}
