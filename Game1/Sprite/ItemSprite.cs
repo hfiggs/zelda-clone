0c4c0c4bcbed41e6d9fd1d47d757d4c85b25d9f1
Sergei Fedulov <fedulov.1@osu.edu>
2020-09-18 14:40:31 -0400

    Added ItemSpriteFactory class and ItemSprite class to draw sprites. Also added updated to a formatted item sprite sheet.

---
+++ Game1/Sprite/ItemSprite.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Sprite
+{
+    class ItemSprite : ISprite
+    {
+        private Texture2D texture;
+        private int row;
+        private int column;
+        private int columns = 10;
+        private int rows = 4;
+        public ItemSprite(Texture2D texture, int column, int row)
+        {
+            this.texture = texture;
+            this.row = row;
+            this.column = column;
+        }
+
+        public void Update()
+        {
+        }
+
+       public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            int width = texture.Width / columns;
+            int height = texture.Height / rows;
+
+            Console.WriteLine(width + "+" + height);
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, width, height);
+
+            spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.White);
+        }
+    }
+    
+}
+
