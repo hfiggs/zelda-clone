9635f8c2aff4ea373a8bc2e5930bbad4797ccba5
JeffreyGaydos <jeff13gaydos@gmail.com>
2020-09-21 17:52:57 -0400

    Added Environment Factory with first factory method

---
+++ Game1/Environment/EnvironmentSpriteFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Game1.Sprite;
+
+
+namespace Game1.Environment
+{
+	public class EnvironmentSpriteFactory
+	{
+		private Texture2D doors;
+		private Texture2D tiles;
+
+		public Instance = new EnvironmentSpriteFactory();
+
+		private EnvironmentSpriteFactory()
+		{
+		}
+
+		public LoadContent()
+		{
+			this.doors = content.Load<Texture2D>("images/ss_doors");
+			this.tiles = content.Load<Texture2D>("images/ss_tiles");
+		}
+
+		public ISprite createFloor(Vector2 position)
+        {
+			return new NotAnimatedNotMovingSprite(tiles, 0, 0, position);
+        }
+	}
+}
