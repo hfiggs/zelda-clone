aaebb068d41702f115b2a83ad0a919fca502010b
JeffreyGaydos <jeff13gaydos@gmail.com>
2020-09-26 10:32:13 -0400

    Comitting branch to merge from master, unfinished

---
+++ Game1/Environment/DoorTop.cs
@@ -0,0 +1,38 @@
+﻿using Game1.Sprite;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Environment
+{
+    class DoorTop : IEnvironment
+    {
+        private SpriteSheet spriteSheet;
+        public enum DoorType { Blank, Open, Locked, Blocked, Hole};
+
+        private DoorType doorType;
+        
+
+        public DoorTop(SpriteSheet spriteSheet, DoorType doorType)
+        {
+            this.spriteSheet = spriteSheet;
+            this.doorType = doorType;
+        }
+
+        public void Draw(SpriteBatch spritebatch, Vector2 position)
+        {
+            Rectangle sourceRect = spriteSheet.PickSprite(0, 0);
+            Rectangle destRect = new Rectangle(position.ToPoint(), new Point(sourceRect.Width, sourceRect.Height));
+            spritebatch.Draw(spriteSheet.GetTexture(), destRect, sourceRect, Color.White);
+        }
+
+        public void Update()
+        {
+            //Extensible later
+        }
+    }
+}
---
+++ Game1/Environment/DoorTopBlank.cs
@@ -0,0 +1,33 @@
+﻿using Game1.Sprite;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Environment
+{
+    class DoorTopBlank : IEnvironment
+    {
+        SpriteSheet spriteSheet;
+
+        public DoorTopBlank(SpriteSheet spriteSheet)
+        {
+            this.spriteSheet = spriteSheet;
+        }
+
+        public void Draw(SpriteBatch spritebatch, Vector2 position)
+        {
+            Rectangle sourceRect = spriteSheet.PickSprite(0, 0);
+            Rectangle destRect = new Rectangle(position.ToPoint(), new Point(sourceRect.Width, sourceRect.Height));
+            spritebatch.Draw(spriteSheet.GetTexture(), destRect, sourceRect, Color.White);
+        }
+
+        public void Update()
+        {
+            //Extensible later
+        }
+    }
+}
