e259f37319926060ba89b02e83f80ae89a223039
Patrick Haughn <haughn.28@buckeyemail.osu.edu>
2020-10-14 12:42:54 -0400

    Created outline for collision detector and collision class

---
+++ Game1/CollisionDetection/Collision.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Game1.CollisionDetection
+{
+    class Collision
+    {
+        private char side;
+        private Rectangle intersectionRec;
+        private Object collider;
+        private Object collidee;
+
+        public Collision(char side, Rectangle intersectionRec, Object collider, Object collidee)
+        {
+            this.side = side;
+            this.intersectionRec = intersectionRec;
+            this.collider = collider;
+            this.collidee = collidee;
+        }
+
+        public char GetSide()
+        {
+            return side;
+        }
+
+        public Rectangle GetRectangle()
+        {
+            return intersectionRec;
+        }
+
+        public Object GetCollider()
+        {
+            return collider;
+        }
+
+        public Object GetCollidee()
+        {
+            return collidee;
+        }
+    }
+}
---
+++ Game1/CollisionDetection/CollisionDetector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game1.CollisionDetection
+{
+    class CollisionDetector
+    {
+        private IRoom room;
+        private List<Collision> collisionList;
+        private Object collider, collidee;
+
+        public CollisionDetector(IRoom room) 
+        {
+            this.room = room;
+        }
+
+        public List<Collision> GetCollisionList()
+        {
+            /* Cycle through room lists
+             * if collision is true
+             *      find intersection rec
+             *      determine side
+             *      create and add collision to list
+             */ 
+
+            return collisionList;
+        }
+    }
+}
