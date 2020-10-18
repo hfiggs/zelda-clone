21ea8de97bea84b2f3ec7be36ee5ddff3b347fc9
Jared Perkins <perkins.578@buckeyemail.osu.edu>
2020-10-18 12:43:14 -0400

    Added Collision Handler Class

---
+++ Game1/Collision_Handling/CollisionHandler.cs
@@ -0,0 +1,17 @@
+﻿using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Collision_Handling
+{
+    static class CollisionHandler
+    {
+        public void handleCollision(List<Collision> collision)
+        {
+
+        }
+    }
+}
