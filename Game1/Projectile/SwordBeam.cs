fea2815ba87efda0a8552b214ef6e6e998fdc363
Patrick Haughn <haughn.28@buckeyemail.osu.edu>
2020-10-23 12:33:15 -0400

    Fixed SwordBeam positioning and added delay

--- Game1/Projectile/SwordBeam.cs
+++ Game1/Projectile/SwordBeam.cs
@@ -6,48 +6,52 @@ namespace Game1.Projectile
     class SwordBeam : IProjectile
     {
         private int columnModifier, counter, rowModifier;
-        private float moveSpeed;
+        private float totalTime;
         private char direction; // 'N' = North, 'S' = South, 'W' = West, 'E' = East
+        private const float moveSpeed = 250;
+        private const float delay = 200; // miliseconds
         private ProjectileSpriteSheet sprite;
         private Vector2 position;
 
         public SwordBeam(char direction, Vector2 position)
         {
             this.direction = direction;
-            this.position = position;
+            this.position = position + new Vector2(-2, 3);
             sprite = ProjectileSpriteFactory.Instance.CreateSwordBeamSprite();
-            moveSpeed = 500;
             columnModifier = 0;
             rowModifier = 0;
             counter = 0;
+            totalTime = 0;
         }
         public bool Update(GameTime gameTime)
         {
-            if (direction == 'N') {
-                position.Y -= moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                rowModifier = 0;
-            } else if (direction == 'S') {
-                position.Y += moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                rowModifier = 1;
-            } else if (direction == 'W') {
-                position.X -= moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                rowModifier = 2;
-            } else if (direction == 'E') {
-                position.X += moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                rowModifier = 3;
-            }
-
-            // Used to change sprite sheet column and allow for flashing
-            if (counter == 5) {
-                if (columnModifier == 0) {
-                    columnModifier = 1;
-                } else {
-                    columnModifier = 0;
+            totalTime += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+            if (totalTime >= delay) {
+                if (direction == 'N') {
+                    position.Y -= moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    rowModifier = 0;
+                } else if (direction == 'S') {
+                    position.Y += moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    rowModifier = 1;
+                } else if (direction == 'W') {
+                    position.X -= moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    rowModifier = 2;
+                } else if (direction == 'E') {
+                    position.X += moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    rowModifier = 3;
                 }
 
-                counter = 0;
-            } else {
-                counter++;
+                // Used to change sprite sheet column and allow for flashing
+                if (counter == 5) {
+                    if (columnModifier == 0) {
+                        columnModifier = 1;
+                    } else {
+                        columnModifier = 0;
+                    }
+                    counter = 0;
+                } else {
+                    counter++;
+                }
             }
 
             //for now just return false, able to return true in the future when this needs to be removed from the projectiles list in game.
@@ -55,11 +59,13 @@ namespace Game1.Projectile
         }
         public void Draw(SpriteBatch spriteBatch, Color color)
         {
-            int columnOfSprite = sprite.GetColumnOfSprite();
-            Rectangle sourceRectangle = sprite.PickSprite(columnOfSprite + columnModifier, rowModifier); ;
-            Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, sourceRectangle.Width, sourceRectangle.Height);
+            if (totalTime >= delay) {
+                int columnOfSprite = sprite.GetColumnOfSprite();
+                Rectangle sourceRectangle = sprite.PickSprite(columnOfSprite + columnModifier, rowModifier); ;
+                Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, sourceRectangle.Width, sourceRectangle.Height);
 
-            spriteBatch.Draw(sprite.GetTexture(), destinationRectangle, sourceRectangle, color);
+                spriteBatch.Draw(sprite.GetTexture(), destinationRectangle, sourceRectangle, color);
+            }
         }
 
         public override bool Equals(object obj)
