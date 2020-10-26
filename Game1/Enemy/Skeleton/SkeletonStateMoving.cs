c8122d9d68aa24a2cc895ea105a62e444ce53425
Jared Perkins <perkins.578@buckeyemail.osu.edu>
2020-10-26 00:26:55 -0400

    fixed skeleton decorator

--- Game1/Enemy/Skeleton/SkeletonStateMoving.cs
+++ Game1/Enemy/Skeleton/SkeletonStateMoving.cs
@@ -80,7 +80,7 @@ namespace Game1.Enemy
 
         public void Draw(SpriteBatch spriteBatch, Color color)
         {
-            Sprite.Draw(spriteBatch, position, Color.White);
+            Sprite.Draw(spriteBatch, position, color);
             
         }
 
