c4020f4cd890ecc45b514e18e45b254c269f644b
Jared Perkins <perkins.578@buckeyemail.osu.edu>
2020-10-26 21:20:48 -0400

    bat and hand fix

--- Game1/Command/CollisionHandlerCommands/Enemy To Block Commands/EnemyToBlockWestSideCommand.cs
+++ Game1/Command/CollisionHandlerCommands/Enemy To Block Commands/EnemyToBlockWestSideCommand.cs
@@ -18,15 +18,21 @@ namespace Game1.Command.CollisionHandlerCommands
         public void Execute(Collision collision)
         {
             IEnemy enemy = (IEnemy)collision.collider;
-            Type enviType = ((IEnvironment)collision.collidee).GetType();
             Vector2 movementAmount = new Vector2(-collision.intersectionRec.Width, 0);
-            if(enemy.GetType() != typeof(Bat))
+            if(enemy.GetType() != typeof(Bat) && enemy.GetType() != typeof(Hand))
             {
                 enemy.EditPosition(movementAmount);
             }
-            else if(enviType == typeof(RoomBorder))
+            else
             {
-                enemy.EditPosition(movementAmount);
+                if (collision.intersectionRec.X >= 210)
+                    enemy.EditPosition(new Vector2(-collision.intersectionRec.Width, 0));
+                else if (collision.intersectionRec.Y <= 32)
+                    enemy.EditPosition(new Vector2(0, collision.intersectionRec.Height));
+                else if (collision.intersectionRec.Y >= 135)
+                    enemy.EditPosition(new Vector2(0, -collision.intersectionRec.Height));
+                else if (collision.intersectionRec.X <= 32)
+                    enemy.EditPosition(new Vector2(collision.intersectionRec.Width, 0));
             }
         }
     }
