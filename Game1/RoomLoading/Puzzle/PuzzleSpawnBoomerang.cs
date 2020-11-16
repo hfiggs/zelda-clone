18d3af7a76baa60dbf8d682b8303f50ea923e0ae
Sergei Fedulov <fedulov.1@osu.edu>
2020-11-15 19:23:37 -0500

    added boomerang puzzle

---
+++ Game1/RoomLoading/Puzzle/PuzzleSpawnBoomerang.cs
@@ -0,0 +1,31 @@
+﻿using Game1.Item;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.RoomLoading.Puzzle
+{
+    class PuzzleSpawnBoomerang : IPuzzle
+    {
+        private Boolean complete;
+        public PuzzleSpawnBoomerang()
+        {
+            complete = false;
+        }
+
+        public void Check(GameTime gameTime, Room room)
+        {
+            if (!complete) {
+                if (room.EnemyList.Count == 0)
+                {
+                    room.SpawnItem(new ItemBoomerang(new Vector2(128, 88)));
+                    complete = true;
+                }
+            }
+            
+        }
+    }
+}
