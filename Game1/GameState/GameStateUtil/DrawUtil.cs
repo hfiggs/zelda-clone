4b91d9c48a99f8dcf268255bd4c7f541df4adfce
Hunter <hunter.figgs@gmail.com>
2020-12-09 00:17:40 -0500

    Fixed bug where player was being drawn on top of doors.

--- Game1/GameState/GameStateUtil/DrawUtil.cs
+++ Game1/GameState/GameStateUtil/DrawUtil.cs
@@ -10,42 +10,53 @@ namespace Game1.GameState.GameStateUtil
     {
         private static readonly Color color = Color.White;
 
-        private const float roomOffset = 40f;
+        private static readonly float zeroOffset = 0f;
+        private static readonly float roomOffset = 40f;
         private static readonly float hudOffset = -136f;
 
         public static void DrawScreen(Screen screen, SpriteBatch spriteBatch, IResolutionManager resolutionManager)
         {
-            var drawMatrix = resolutionManager.GetResolutionMatrix();
-
-            drawMatrix.Translation = new Vector3(0, roomOffset * resolutionManager.GetResolutionScale(), 0);
-
-            spriteBatch.Begin(SpriteSortMode.FrontToBack, null, SamplerState.PointClamp, null, null, null, drawMatrix);
+            SetupDraw(spriteBatch, resolutionManager, zeroOffset, roomOffset);
 
             screen.Draw(spriteBatch, color);
 
-            spriteBatch.End();
+            EndDraw(spriteBatch, resolutionManager);
+        }
 
-            ResetTranslation(drawMatrix);
+        public static void DrawRoom(Room room, SpriteBatch spriteBatch, IResolutionManager resolutionManager, Vector2 offset)
+        {
+            SetupDraw(spriteBatch, resolutionManager, offset.X, offset.Y);
+
+            room.Draw(spriteBatch, color);
+
+            EndDraw(spriteBatch, resolutionManager);
         }
 
         public static void DrawHUDOffset(HUDInterface hud, SpriteBatch spriteBatch, IResolutionManager resolutionManager)
         {
-            var drawMatrix = resolutionManager.GetResolutionMatrix();
-
-            drawMatrix.Translation = new Vector3(0, hudOffset * resolutionManager.GetResolutionScale(), 0);
-
-            spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, drawMatrix);
+            SetupDraw(spriteBatch, resolutionManager, zeroOffset, hudOffset);
 
             hud.Draw(spriteBatch, new Vector2(0, 0), color);
 
-            spriteBatch.End();
-
-            ResetTranslation(drawMatrix);
+            EndDraw(spriteBatch, resolutionManager);
         }
 
-        private static void ResetTranslation(Matrix drawMatrix)
+        private static void EndDraw(SpriteBatch spriteBatch, IResolutionManager resolutionManager)
         {
+            var drawMatrix = resolutionManager.GetResolutionMatrix();
+
             drawMatrix.Translation = new Vector3(0, 0, 0);
+
+            spriteBatch.End();
+        }
+
+        private static void SetupDraw(SpriteBatch spriteBatch, IResolutionManager resolutionManager, float xOffset = 0f, float yOffset = 0f)
+        {
+            var drawMatrix = resolutionManager.GetResolutionMatrix();
+
+            drawMatrix.Translation = new Vector3(xOffset * resolutionManager.GetResolutionScale(), yOffset * resolutionManager.GetResolutionScale(), 0);
+
+            spriteBatch.Begin(SpriteSortMode.FrontToBack, null, SamplerState.PointClamp, null, null, null, drawMatrix);
         }
     }
 }
