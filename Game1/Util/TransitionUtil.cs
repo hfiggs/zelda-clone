ee6aae03c234a91117d7e91cf3b7d6603c9efdc8
Hunter <hunter.figgs@gmail.com>
2021-03-24 21:36:32 -0400

    Removed unnecessary using statements from TransitionUtil.

--- Game1/Util/TransitionUtil.cs
+++ Game1/Util/TransitionUtil.cs
@@ -1,12 +1,5 @@
 ﻿/* Author: Hunter Figgs.3 */
 
-using Game1.Environment;
-using Game1.GameState;
-using Game1.Player;
-using Game1.RoomLoading;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace Game1.Util
 {
     public static class TransitionUtil
